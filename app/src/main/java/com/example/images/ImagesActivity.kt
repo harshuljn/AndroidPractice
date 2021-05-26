0b3e438456446073ed4e16d126808f9a14fb13cd
Harshul Jain <harjain@expediagroup.com>
2021-05-26 11:32:27 +0530

    Test run single image

--- app/src/main/java/com/example/images/ImagesActivity.kt
+++ app/src/main/java/com/example/images/ImagesActivity.kt
@@ -2,20 +2,11 @@ package com.example.images
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListAdapter
-import android.widget.ListView
-import android.widget.TextView
+import android.widget.*
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import org.json.JSONArray
-import org.w3c.dom.Text
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import com.bumptech.glide.Glide
 
 class ImagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +28,10 @@ class ImagesActivity : AppCompatActivity() {
                 val imagesList = findViewById<ListView>(R.id.image_list)
                 val listAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, url_list)
                 imagesList.adapter = listAdapter
+
+                val testImageView = findViewById<ImageView>(R.id.testImageView)
+                Glide.with(this).load(url_list[0])
+                    .into(testImageView);
             },
             {})
 
